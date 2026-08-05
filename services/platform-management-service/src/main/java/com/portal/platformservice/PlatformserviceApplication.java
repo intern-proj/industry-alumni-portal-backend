1fb598d5283886857adea25c0a77ebe91e2da6fa
Rusiru Hasan <rusiruhasan0823@gmail.com>
2026-08-05 13:22:44 +0530

    SpringBoot project & dependancies

---
+++ services/platform-management-service/src/main/java/com/portal/platformservice/PlatformserviceApplication.java
@@ -0,0 +1,13 @@
+package com.portal.platformservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformserviceApplication.class, args);
+	}
+
+}
---
+++ services/platform-management-service/src/test/java/com/portal/platformservice/PlatformserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.platformservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
