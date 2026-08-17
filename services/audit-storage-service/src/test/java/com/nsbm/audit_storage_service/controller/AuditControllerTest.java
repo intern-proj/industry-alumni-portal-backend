444f1022cd4b63993127e557d7e8a09294a6c5a8
Prasad Prasanna <prasadkvithana@gmail.com>
2026-08-17 15:14:21 +0530

    fix: correct @WebMvcTest/@AutoConfigureMockMvc import package in all test files
    Wrong:  org.springframework.boot.webmvc.test.autoconfigure (does not exist)
    Right:  org.springframework.boot.test.autoconfigure.web.servlet
    
    Fixed 8 test files:
    - application-service: JobApplicationControllerTest
    - audit-storage-service: AuditControllerTest, StorageControllerTest
    - event-management-service: AgendaControllerTest, CertificateEligibilityCriteriaControllerTest,
      EventControllerTest, GuestSpeakerControllerTest, VenueControllerTest

--- services/audit-storage-service/src/test/java/com/nsbm/audit_storage_service/controller/AuditControllerTest.java
+++ services/audit-storage-service/src/test/java/com/nsbm/audit_storage_service/controller/AuditControllerTest.java
@@ -8,7 +8,7 @@ import com.nsbm.audit_storage_service.service.AuditService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
